556275c74cdd55f8d8e72d43e4482010a37fda16
asermartinez <aser.martinez@gmail.com>
2018-04-17 01:17:46 +0200

    refactor

--- rovarspraket.py
+++ rovarspraket.py
@@ -1,11 +1,10 @@
-# coding=utf-8
 
-word = input("\nWrite a word/sentence to translate to Rövarspråket: ")
 
 consonants = "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz"
 
 
-def translate(word):  # Rövarspråket secret language translator
+def translate():  # Rövarspråket secret language translator
+    word = input("\nWrite a word/sentence to translate to Rövarspråket: ")
     new_word = []
     for x in word:
         if x in consonants and x.islower():
@@ -15,7 +14,33 @@ def translate(word):  # Rövarspråket secret language translator
         else:
             new_word.append(x)
     new_word = "".join(new_word)
+    print("\nTranslating {} to Rövarspråket: {}".format(word, new_word))
     return new_word
 
 
-print("\nTranslating {} to Rövarspråket: {}".format(word, translate(word)))
+def decode():
+    word = input("\nWrite a word/sentence to decode from Rövarspråket secret language: ")
+    new_word = ""
+    index = 0
+    while index in range(len(word)):
+        new_word += word[index]
+        if word[index] in consonants:
+            index += 3
+        else:
+            index += 1
+    print("\n"+new_word)
+    return new_word
+
+
+while True:
+    try:
+        answer = int(input("\n(1)-Code , (2)-Decode, (3)-Exit : "))
+        if answer == 1:
+            translate()
+        if answer == 2:
+            decode()
+        if answer == 3:
+            break
+    except ValueError:
+        print("Try again...")
+
